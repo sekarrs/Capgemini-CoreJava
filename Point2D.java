23153c3c54645d5cb27eeef8e2524bf5bc816476
sekarrs <53087869+sekarrs@users.noreply.github.com>
2021-10-11 18:08:02 +0530

    Add files via upload
    First Assignment of Core Java Training

---
+++ Point2D.java
@@ -0,0 +1,13 @@
+package CoreJava;
+
+public class Point2D {
+public int x,y,i,j;
+public Point2D(int i,int j) {
+x=i;
+y=j;
+}
+public String getDetails() {
+System.out.println("point_details:"+x+" "+y);
+return null;
+}
+}
---
+++ TestDetails.java
@@ -0,0 +1,9 @@
+package CoreJava;
+
+public class TestDetails {
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+Point2D B1= new Point2D(22,12);
+B1.getDetails();
+}
+}
